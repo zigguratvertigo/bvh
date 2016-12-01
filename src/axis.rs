d5fa71ac6ac77ab107dc746a1195717bfe4c94b3
Sascha Graeff <sascha@blackicegames.de>
2016-12-01 11:46:17 +0100

    Add Display impl to Axis

--- src/axis.rs
+++ src/axis.rs
@@ -1,6 +1,7 @@
 //! Axis enum for AABBs and AAPs.
 
 use std::ops::Index;
+use std::fmt::{Display, Formatter, Result};
 
 /// An Axis in a three-dimensional coordinate system.
 /// Used to access `Vector3`/`Point3` structs via index.
@@ -22,5 +23,18 @@ impl Index<Axis> for [f32] {
 
     fn index(&self, axis: Axis) -> &f32 {
         self.index(axis as usize)
+    }
+}
+
+/// TODO comment
+///
+impl Display for Axis {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        let c = match *self {
+            Axis::X => "x",
+            Axis::Y => "y",
+            Axis::Z => "z",
+        };
+        write!(f, "{}", c)
     }
 }
